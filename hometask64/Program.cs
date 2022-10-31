a813267df8672668da9e89b769b9967fe8aabcb6
mukhinvictor777 <mukhinvictor@gmail.com>
2022-11-01 03:23:20 +0600

    Осталась только функция Аккермана

--- hometask64/Program.cs
+++ hometask64/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿int getNumberFromUser(string userInformation)
+{
+    int  result = 0;
+    while (result == 0 || result < 1)
+    {
+        Console.Write(userInformation);
+        string userLine = Console.ReadLine();
+        int.TryParse(userLine, out result);
+        if (result == 0 || result < 1) Console.WriteLine($"Введите целое положительное число, вы вввели {userLine}"); 
+        else break;
+    }
+    return result;
+}
+
+void numberRange(int N)
+{
+    if (N == 0)
+    {
+        return;
+    }
+    Console.Write($"{N}, ");
+    numberRange(N - 1);
+}
+
+Console.Write("Введите значение N: ");
+int userNumber = getNumberFromUser("");
+Console.Write($"N = {userNumber} -> ");
+numberRange(userNumber);
+Console.WriteLine();
--- hometask66/Program.cs
+++ hometask66/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿int getNumberFromUser(string userInformation)
+{
+    int  result = 0;
+    while (result == 0 || result < 1)
+    {
+        Console.Write(userInformation);
+        string userLine = Console.ReadLine();
+        int.TryParse(userLine, out result);
+        if (result == 0 || result < 1) Console.WriteLine($"Введите целое положительное число, вы вввели {userLine}"); 
+        else break;
+    }
+    return result;
+}
+
+int sumNumbersFromMtoN(int M, int N)
+{
+    if (N == M)
+    {
+        return M;
+    }
+    else
+    {
+        return N + sumNumbersFromMtoN(M, N - 1);
+    }
+}
+
+Console.Write("Введите значение M: ");
+int userNumberM = getNumberFromUser("");
+Console.Write("Введите значение N: ");
+int userNumberN = getNumberFromUser("");
+if (userNumberM > userNumberN)
+{
+    int buffer = userNumberN;
+    userNumberN = userNumberM;
+    userNumberM = buffer;
+}
+int sum = sumNumbersFromMtoN (userNumberM, userNumberN);
+Console.Write($"M = {userNumberM}; N = {userNumberN} -> {sum}");
+Console.WriteLine();
