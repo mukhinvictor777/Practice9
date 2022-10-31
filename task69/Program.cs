9eea225f48847f5708f65f5863671162f87cd96c
mukhinvictor777 <mukhinvictor@gmail.com>
2022-11-01 03:14:01 +0600

    task 69 done

--- task69/Program.cs
+++ task69/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿int getNumberFromUser(string userInformation)
+{
+    int  result = 0;
+    while (result == 0 || result < 1)
+    {
+        Console.Write(userInformation);
+        string userLine = Console.ReadLine();
+        int.TryParse(userLine, out result);
+        if (result == 0 || result < 1) Console.WriteLine($"Введите целое положительное число, вы вввели {userLine}"); 
+        else break;
+    }
+    return result;
+}
+
+int AvStepeniB(int A, int B)
+{
+    if (B == 0)
+    {
+        
+        return 1;
+    }
+    else
+    {
+        return A * AvStepeniB(A, B - 1);
+    }
+}
+
+Console.Write("Введите значение число A: ");
+int A = getNumberFromUser("");
+Console.Write("Введите значение число B: ");
+int B = getNumberFromUser("");
+int stepenAB = AvStepeniB(A, B);
+Console.Write($"A = {A}; B = {B} -> {stepenAB} ({A} ^ {B})");
+Console.WriteLine();
